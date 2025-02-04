90dc8c8e67b53b8e9a8810f85d8c3df58fee30ed
Isabel Maskrey <isabel@satyrn.dev>
2025-02-04 18:32:41 -0500

    Add countdowns

--- restart.sh
+++ restart.sh
@@ -6,7 +6,15 @@ touch .restart_flag
 
 # Save the server
 ./stuff.sh save-all
-echo "Waiting for server save to complete..."
-sleep 30s
+
+./stuff.sh say Server will go down for restart in 30 seconds.
+sleep 14s
+./stuff.sh say Server will go down for restart in 15 seconds.
+sleep 9s
+./stuff.sh say Server will go down for restart in 5 seconds.
+./stuff.sh say Server will go down for restart in 4 seconds.
+./stuff.sh say Server will go down for restart in 3 seconds.
+./stuff.sh say Server will go down for restart in 2 seconds.
+./stuff.sh say Server will go down for restart in 1 seconds.
 
 ./stuff.sh stop
--- stop.sh
+++ stop.sh
@@ -7,8 +7,13 @@ rm .restart_flag &>/dev/null || true
 
 # Save the server
 ./stuff.sh save-all
-
-echo "Waiting for server save to complete..."
-sleep 30s
-
+./stuff.sh say Server will shut down in 30 seconds.
+sleep 14s
+./stuff.sh say Server will shut down in 15 seconds.
+sleep 9s
+./stuff.sh say Server will shut down in 5 seconds.
+./stuff.sh say Server will shut down in 4 seconds.
+./stuff.sh say Server will shut down in 3 seconds.
+./stuff.sh say Server will shut down in 2 seconds.
+./stuff.sh say Server will shut down in 1 seconds.
 ./stuff.sh stop
