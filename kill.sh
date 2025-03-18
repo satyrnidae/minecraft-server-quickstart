09bc663979517c076ff4e8785151e5c9f3805eea
Isabel Maskrey <isabel@satyrn.dev>
2025-03-18 19:09:05 -0400

    🐛 wrap in process check I am testing this stuff on another machine so that's why there are so many commits lol

--- kill.sh
+++ kill.sh
@@ -9,34 +9,19 @@ echo "Sending SIGTERM to screen process and resuming..."
 
 runshpid=$(sudo -u $RUNAS ps h --ppid $(sudo -u $RUNAS screen -ls | grep $SCREEN | cut -d. -f1) -o pid)
 
-sudo -u $RUNAS kill -TERM $runshpid
-echo Waiting 1 minute for process exit...
-sleep 1s
-for ((i=59;i>0;i--)); do
-    if [ $i -eq 30 ]; then
-        echo Checking for process status in 30 seconds...
-    elif [ $i -eq 15 ]; then
-        echo Checking for process status in 15 seconds...
-    elif [ $i -eq 10 ]; then
-        echo Checking for process status in 10 seconds...
-    elif [ $i -le 5 ]; then
-        if [ $i -eq 1 ]; then
-            echo Checking for process status in 1 second...
-        else
-            echo Checking for process status in $i seconds...
-        fi
-    fi
-    sleep 1s
-done
-
 if ps -p $runshpid >/dev/null
 then
-    echo Kill failed, sending SIGKILL.
-    sudo -u $RUNAS kill -KILL $runshpid
-    echo Waiting 10 seconds for process death...
+    sudo -u $RUNAS kill -TERM $runshpid
+    echo Waiting 1 minute for process exit...
     sleep 1s
-    for ((i=9;i>0;i--)); do
-        if [ $i -le 5 ]; then
+    for ((i=59;i>0;i--)); do
+        if [ $i -eq 30 ]; then
+            echo Checking for process status in 30 seconds...
+        elif [ $i -eq 15 ]; then
+            echo Checking for process status in 15 seconds...
+        elif [ $i -eq 10 ]; then
+            echo Checking for process status in 10 seconds...
+        elif [ $i -le 5 ]; then
             if [ $i -eq 1 ]; then
                 echo Checking for process status in 1 second...
             else
@@ -45,14 +30,34 @@ then
         fi
         sleep 1s
     done
+
     if ps -p $runshpid >/dev/null
     then
-        echo Failed to terminate run script! Reattaching screen process.
+        echo Kill failed, sending SIGKILL.
+        sudo -u $RUNAS kill -KILL $runshpid
+        echo Waiting 10 seconds for process death...
+        sleep 1s
+        for ((i=9;i>0;i--)); do
+            if [ $i -le 5 ]; then
+                if [ $i -eq 1 ]; then
+                    echo Checking for process status in 1 second...
+                else
+                    echo Checking for process status in $i seconds...
+                fi
+            fi
+            sleep 1s
+        done
+        if ps -p $runshpid >/dev/null
+        then
+            echo Failed to terminate run script! Reattaching screen process.
+        else
+            echo Run script terminated. Reattaching screen process in case it is still active.
+        fi
     else
         echo Run script terminated. Reattaching screen process in case it is still active.
     fi
-else
-    echo Run script terminated. Reattaching screen process in case it is still active.
-fi
 
-sudo -u $RUNAS screen -r $SCREEN
+    sudo -u $RUNAS screen -r $SCREEN
+else
+    echo No process to kill could be found.
+fi
