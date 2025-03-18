1fd805475c0d6ae0e431ebdd2fedd38efbc9525a
Isabel Maskrey <isabel@satyrn.dev>
2025-03-18 19:03:44 -0400

    ✨ More robust kill script Panic sigkill if sigterm fails

--- kill.sh
+++ kill.sh
@@ -6,6 +6,53 @@ cd "$(dirname "$0")"
 . ./env.sh
 
 echo "Sending SIGTERM to screen process and resuming..."
-sudo -u $RUNAS kill -TERM $(sudo -u $RUNAS ps h --ppid $(sudo -u $RUNAS screen -ls | grep $SCREEN | cut -d. -f1) -o pid)
+
+runshpid=$(sudo -u $RUNAS ps h --ppid $(sudo -u $RUNAS screen -ls | grep $SCREEN | cut -d. -f1) -o pid)
+
+sudo -u $RUNAS kill -n 15 $runshpid
+echo Waiting 1 minute for process exit...
+sleep 1s
+for ((i=59;i>0;i++)); do
+    if [ $i -eq 30 ]; then
+        echo Checking for process status in 30 seconds...
+    elif [ $i -eq 15 ]; then
+        echo Checking for process status in 15 seconds...
+    elif [ $i -eq 10 ]; then
+        echo Checking for process status in 10 seconds...
+    elif [ $i -le 5 ]; then
+        if [ $i -eq 1 ]; then
+            echo Checking for process status in 1 second...
+        else
+            echo Checking for process status in $i seconds...
+        fi
+    fi
+    sleep 1s
+done
+
+if ps -p $runshpid >/dev/null
+then
+    echo Kill failed, sending SIGKILL.
+    sudo -u $RUNAS kill -n 9 $runshpid
+    echo Waiting 10 seconds for process death...
+    sleep 1s
+    for ((i=9;i>0;i--)); do
+        if [ $i -le 5 ]; then
+            if [ $i -eq 1 ]; then
+                echo Checking for process status in 1 second...
+            else
+                echo Checking for process status in $i seconds...
+            fi
+        fi
+        sleep 1s
+    done
+    if ps -p $runshpid >/dev/null
+    then
+        echo Failed to terminate run script! Reattaching screen process.
+    else
+        echo Run script terminated. Reattaching screen process in case it is still active.
+    fi
+else
+    echo Run script terminated. Reattaching screen process in case it is still active.
+fi
+
 sudo -u $RUNAS screen -r $SCREEN
-echo "Screen terminated."
